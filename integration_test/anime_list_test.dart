499347bbb89ab916cdc951af5acb58fb2688bc2b
Daditoidk <Daditoidk@users.noreply.github.com>
2025-11-27 11:08:02 -0500

    test(ainme list integration test) polished test, passed

--- integration_test/anime_list_test.dart
+++ integration_test/anime_list_test.dart
@@ -1,4 +1,5 @@
 import 'package:anime_discovery_app_v2/main.dart' as app;
+import 'package:anime_discovery_app_v2/views/widgets/anime_card.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:integration_test/integration_test.dart';
@@ -11,13 +12,14 @@ void main() {
 
       //Wait for loading first state
       await tester.pump();
+      await tester.pumpAndSettle();
 
 
 //Should find a list of animes
-      expect(find.byType(SliverList), findsOneWidget);
+      expect(find.byType(ListView), findsOneWidget);
 
 //Should find at least one anime item
-      expect(find.byType(AnimeTile), findsAtLeast(1));
+      expect(find.byType(AnimeCard), findsAtLeast(1));
 
 //The animeTile should have title and rating
       expect(find.textContaining('★'), findsWidgets);
